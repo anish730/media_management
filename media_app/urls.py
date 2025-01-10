35c8b896083d93725b23a0d73353ba6427a94f0b
anish730 <anish.pdl09@gmail.com>
2025-01-10 19:21:14 +0545

    new changes

--- media_app/urls.py
+++ media_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.media_management, name='upload_files'),
     path('files/<int:file_id>/download/', views.download_file, name='download_file'),
